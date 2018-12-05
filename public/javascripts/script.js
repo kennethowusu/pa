40d78837c97289a15808842f6e645e38fb28cd46
Kenneth Owusu <keninchrist4eva@gmail.com>
2018-12-05 13:10:28 +0000

    commit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,39 +1,39 @@
-// //==================FOR STICKY HEADER========================//
-// $('.header').sticky();
-//
-// $('.header').on('sticky-start',function(){
-//   $('.header').toggleClass('header-sticky');
-//   $('.nav-link').toggleClass('links-sticky');
-//   $('.burger1,.burger2,.burger3').addClass('burger-sticky');
-//   $('.hamburger-text,.sublist a').addClass('hamburger-text-sticky');
-//   $('.hamburger').addClass('hamburger-sticky');
-// })
-//
-// $('.header').on('sticky-end',function(){
-//   $('.header').removeClass('header-sticky');
-//   $('.nav-link').removeClass('links-sticky');
-//   $('.burger1,.burger2,.burger3').removeClass('burger-sticky');
-//   $('.hamburger-text,.sublist a').removeClass('hamburger-text-sticky');
-//   $('.hamburger').removeClass('hamburger-sticky');
-// })
-//
-//
-//
-//
-// //==================FOR HAMBURGER=====================//
-//
-//  const showMenu =  ()=>{
-//   $('.js-hamburger').on('click',()=>{
-//     $('body').toggleClass('translate');
-//     $('.burger-cont').toggleClass('p-relative');
-//     $('.burger1,.burger2,.burger3').toggleClass('change');
-//     $('body').toggleClass('control-body');
-//   });
-// }
-//
-// // showMenu();
-//
-//
+//==================FOR STICKY HEADER========================//
+$('.header').sticky();
+
+$('.header').on('sticky-start',function(){
+  $('.header').toggleClass('header-sticky');
+  $('.nav-link').toggleClass('links-sticky');
+  $('.burger1,.burger2,.burger3').addClass('burger-sticky');
+  $('.hamburger-text,.sublist a').addClass('hamburger-text-sticky');
+  $('.hamburger').addClass('hamburger-sticky');
+})
+
+$('.header').on('sticky-end',function(){
+  $('.header').removeClass('header-sticky');
+  $('.nav-link').removeClass('links-sticky');
+  $('.burger1,.burger2,.burger3').removeClass('burger-sticky');
+  $('.hamburger-text,.sublist a').removeClass('hamburger-text-sticky');
+  $('.hamburger').removeClass('hamburger-sticky');
+})
+
+
+
+
+//==================FOR HAMBURGER=====================//
+
+ const showMenu =  ()=>{
+  $('.js-hamburger').on('click',()=>{
+    $('body').toggleClass('translate');
+    $('.burger-cont').toggleClass('p-relative');
+    $('.burger1,.burger2,.burger3').toggleClass('change');
+    $('body').toggleClass('control-body');
+  });
+}
+
+showMenu();
+
+
 
 const errors = [];
 const firstname         = $('.firstname');
