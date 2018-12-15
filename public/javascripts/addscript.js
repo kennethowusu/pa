996e97e32379990c34361d30c030d76bd8b7caf2
Kenneth Owusu <keninchrist4eva@gmail.com>
2018-12-15 15:06:25 +0000

    commit

--- public/javascripts/addscript.js
+++ public/javascripts/addscript.js
@@ -22,18 +22,23 @@ $('.clipboard').on('click',function(){
 
 
 //======================for payment=============//
-const displayPaymentOption = function(){
-  const card_payment_section = $('.card-payement-section');
-  const paypal_payment_section = $('.paypal-payment-section');
+const card_payment_section = $('.card-payment-section');
+const paypal_payment_section = $('.paypal-payment-section');
+
+const displayPaymentOptionAction =  function(){
   if($('#card-option').is(':checked')) {
-    // paypal_payment_section.hide();
-    // card_payment_section.show();
-    console.log('card is checked')
+    paypal_payment_section.hide();
+    card_payment_section.show();
    }
   else if($('#paypal-option').is(':checked')){
-    // card_payment_section.hide();
-    // paypal_payment_section.show();
-    console.log('paypal is checked')
+    card_payment_section.hide();
+    paypal_payment_section.show();
   }
 }
+const displayPaymentOption = function(){
+  $('#card-option,#paypal-option').on('click',function(){
+     displayPaymentOptionAction();
+  })
+}
+displayPaymentOptionAction();
 displayPaymentOption();
