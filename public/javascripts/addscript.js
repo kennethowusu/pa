e441f0294fcec8d2d195ffb78cc0602e03ad35e1
Kenneth Owusu <keninchrist4eva@gmail.com>
2018-12-15 13:49:44 +0000

    commit

--- public/javascripts/addscript.js
+++ public/javascripts/addscript.js
@@ -19,3 +19,21 @@ $('.clipboard').on('click',function(){
 
   });
 })
+
+
+//======================for payment=============//
+const displayPaymentOption = function(){
+  const card_payment_section = $('.card-payement-section');
+  const paypal_payment_section = $('.paypal-payment-section');
+  if($('#card-option').is(':checked')) {
+    // paypal_payment_section.hide();
+    // card_payment_section.show();
+    console.log('card is checked')
+   }
+  else if($('#paypal-option').is(':checked')){
+    // card_payment_section.hide();
+    // paypal_payment_section.show();
+    console.log('paypal is checked')
+  }
+}
+displayPaymentOption();
