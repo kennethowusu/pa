5560cc2eda0c2b7dfd699482069bbc2d038a1c01
Kenneth Owusu <keninchrist4eva@gmail.com>
2019-11-22 14:47:27 +0000

    configured sass in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,17 +5,18 @@ module.exports = function(grunt) {
     //=============configuring sass================//
 
     //======================configuring autoprefixer===================//
-    // autoprefixer:{
-    //     options:{
-    //         // We need to `freeze` browsers versions for testing purposes.
-    //         browsers: ['opera 12', 'ff 15', 'chrome 25']
-    //     },
-    //
-    //      style: {
-    //       src: 'public/stylesheets/style.css',
-    //       dest: 'public/stylesheets/style.css'
-    //     }
-    // },
+    sass: {                              // Task
+        dist: {                            // Target
+          options: {                       // Target options
+            style: 'expanded'
+          },
+          files: {                         // Dictionary of files
+            //'dist/css/bootstrap.css': 'node_modules/bootstrap/scss/bootstrap.scss',
+              'dist/css/primeaxis.css':'src/scss/primeaxis/primeaxis.scss',
+              'dist/css/general.css':'src/scss/general/general.scss'
+          }
+        }
+      },
 
     //=========================configuring watch=================//
     watch: {
@@ -23,70 +24,10 @@ module.exports = function(grunt) {
          livereload:false
        },
        sass: {
-         files: ['sass/**/*'],
-         tasks: ['sass','autoprefixer']
-       },
-       js:{
-         files:['js/**/*','scripts/**/**/**/**'],
-         tasks:['concat']
+         files: ['src/scss/**/*'],
+         tasks: ['sass']
        }
     },
-    concat: {
-  options: {
-    separator: ';',
-  },
-  dist: {
-    src: ['js/account-universal.js',
-           'js/main.js',
-           'js/remodal.js',
-           'js/withdraw.js'
-
-          ],
-    dest: 'public/javascripts/addscript.js',
-  },
-  home:{
-    src:[
-       'js/vendors/jquery-3.2.1.min.js',
-      'js/vendors/jquery-migrate-3.0.1.min.js',
-      'js/vendors/popper.min.js',
-      'js/vendors/bootstrap.min.js',
-      'js/vendors/jquery.easing.1.3.js',
-      'js/vendors/jquery.waypoints.min.js',
-      'js/vendors/jquery.stellar.min.js',
-      'js/vendors/owl.carousel.min.js',
-      'js/vendors/jquery.magnific-popup.min.js',
-      'js/vendors/aos.js',
-      'js/vendors/jquery.animateNumber.min.js',
-      'js/vendors/bootstrap-datepicker.js',
-      'js/vendors/scrollax.min.js',
-      'js/vendors/universal.js',
-      'js/vendors/main.js',
-      'js/vendors/addscript.js'
-
-    ],
-    dest:'public/javascripts/home.js'
-  },
-
-  account:{
-    src:[
-
-      'js/account/universal.js',
-      // 'js/account/util.js',
-      // 'js/account/modal.js',
-      // 'js/account/popover.js',
-      // 'js/account/dropdown.js',
-      'node_modules/sweetalert/dist/sweetalert.min.js',
-      'js/account/settings.js',
-      'js/account/copy.js',
-      'js/account/investment.js',
-      'js/account/withdraw.js',
-      'js/account/notification.js'
-
-    ],
-    dest:'public/javascripts/account.js'
-  }
-
-},
     //=================================configuring uglify================//
 
       uglify: {
@@ -99,9 +40,6 @@ module.exports = function(grunt) {
           }
         }
       }
-
-
-
   });
 
 
